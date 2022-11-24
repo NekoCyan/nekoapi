03db81d61abc34075732fbc0d90abbe52c2815a7
NekoCyan <cyanhijirikawa88@gmail.com>
2022-11-25 00:21:19 +0700

    fix: can not get URL.

--- index.ts
+++ index.ts
@@ -12,40 +12,56 @@ app.listen(PORT, () => {
 });
 
 function getRandomImageURL(): Promise<string> {
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		axios
 			.get('https://nekos.life/api/v2/img/neko')
 			.then((res) => {
 				resolve(res.data.url);
 			})
-			.catch((e) => {
-				console.log(e.message);
-				resolve('');
+			.catch(() => {
+				const defaultURL =
+					'https://media.discordapp.net/attachments/878276279105884210/1045378789905010758/99083012_p7.jpg';
+				resolve(defaultURL);
 			});
 	});
 }
 
-function getRandomImage(): Promise<{ data: Buffer; end: string }> {
-	return new Promise((resolve) => {
-		getRandomImageURL().then((url) => {
-			axios.get(url, { responseType: 'arraybuffer' }).then((res) => {
-				resolve({
-					data: res.data,
-					end: res.headers['content-type'],
-				});
+function getRandomImage(): Promise<{ data: Buffer; end: string } | null> {
+	return new Promise((resolve, reject) => {
+		getRandomImageURL()
+			.then((url) => {
+				axios
+					.get(url, { responseType: 'arraybuffer' })
+					.then((res) => {
+						resolve({
+							data: res.data,
+							end: res.headers['content-type'],
+						});
+					})
+					.catch(reject);
+			})
+			.catch((e) => {
+				resolve(null);
 			});
-		});
 	});
 }
 
 app.get('/', (req, res) => res.status(200).send('Hello World!')).get(
 	'/api/neko',
 	async (req, res) => {
-		const image = await getRandomImage();
-		res.set({
-			'content-type': image.end,
-			'cache-control': 'max-age=0, no-cache, no-store, must-revalidate',
-		});
-		res.send(image.data);
+		try {
+			const image = await getRandomImage();
+			if (image) {
+				res.set({
+					'content-type': image.end,
+					'cache-control':
+						'max-age=0, no-cache, no-store, must-revalidate',
+				});
+				return res.send(image.data);
+			}
+			throw new Error('Cannot get image.');
+		} catch {
+			res.send({ code: -1, message: 'error' });
+		}
 	},
 );
