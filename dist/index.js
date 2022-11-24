433ccf40490767d7ca8a83a8f1c0099f039fe19e
NekoCyan <cyanhijirikawa88@gmail.com>
2022-11-24 22:14:09 +0700

    Remake and update to 2.0.0.

---
+++ dist/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+function getRandomImageURL() {
+    return new Promise((resolve) => {
+        axios
+            .get('https://nekos.life/api/v2/img/neko')
+            .then((res) => {
+                resolve(res.data.url);
+            })
+            .catch((e) => {
+                console.log(e.message);
+                resolve('');
+            });
+    });
+}
+
+function getRandomImage() {
+    return new Promise((resolve) => {
+        getRandomImageURL().then((url) => {
+            axios.get(url, { responseType: 'arraybuffer' }).then((res) => {
+                resolve({
+                    data: res.data,
+                    end: res.headers['content-type'],
+                });
+            });
+        });
+    });
+}
+
+app
+    .get('/', (req, res) => res.status(200).send('Hello World!'))
+    .get('/api/neko', async (req, res) => {
+        const image = await getRandomImage();
+        res.set({
+            'content-type': image.end,
+            'cache-control': 'max-age=0, no-cache, no-store, must-revalidate',
+        });
+        res.send(image.data);
+    });
+//# sourceMappingURL=index.js.map
